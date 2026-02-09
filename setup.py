9388430df638bbad3d78316b225557d959a308a0
kishore-kix0 <kishoreeddie8@gmail.com>
2026-02-10 01:26:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="kishore",
+    author_email="kishoreeddie8@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
